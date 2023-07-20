e2538d3959fa090b71b7e36938c4d4371737b86a
NLPhuGia <88957832+NLPhuGia@users.noreply.github.com>
2023-07-20 09:23:42 +0700

    hghg

---
+++ ROBOT_R_VS_D.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ROBOT_R_VS_D.ino
+ Created:	6/26/2023 12:42:27 PM
+ Author:	Dell M4800
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Sketch1/Sketch1.ino
@@ -0,0 +1,49 @@
+﻿#include <WiFi.h>
+#include <WebSocketsClient.h>     
+
+const char* ssid = "CHUONG_H";
+const char* password = "vn911989";
+const char* serverAddress = "ws://127.0.0.1:8080"; // Thay thế bằng địa chỉ máy chủ WebSocket
+
+WebSocketsClient webSocket;
+
+void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
+    switch (type) {
+    case WStype_DISCONNECTED:
+        Serial.println("[WebSocket] Disconnected");
+        break;
+    case WStype_CONNECTED:
+        Serial.println("[WebSocket] Connected");
+        break;
+    case WStype_TEXT:
+        Serial.print("[WebSocket] Received message: ");
+        Serial.println((char*)payload);
+        break;
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    // Kết nối với mạng Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+
+    // Thiết lập kết nối WebSocket
+    webSocket.begin(serverAddress);
+    webSocket.onEvent(webSocketEvent);
+}
+
+void loop() {
+    // Kiểm tra và duy trì kết nối WebSocket
+    webSocket.loop();
+
+    // Gửi tin nhắn đến máy chủ WebSocket
+    webSocket.send("Hello, WebSocket!");
+
+    delay(1000);
+}
